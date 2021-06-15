0bd1d23908dfe3ca206db3bf02b560751dfb3e74
Yavuz Sonmez <ysonmez@2-G-3.42heilbronn.de>
2021-06-15 16:54:31 +0200

    fourth commit

--- ft_atoi.c
+++ ft_atoi.c
@@ -13,8 +13,31 @@
 int	ft_atoi(char *str)
 {
 	size_t sign;
-
+	size_t i;
+	int result;
 
 	sign = 0;
+	i = 0;
+	result = 1;
 
+	while((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while(str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			sign++;
+		i++;
+	}
+	while(str[i] == 32)
+		i++;
+	while(str[i] >= 48 && str[i] <= 57)
+	{
+		result = (str[i] - 48)
+		i++;
+	}
+	if (sign % 2 == 1)
+		return (result * (-1);
+	else
+		return (result);
 }
+
