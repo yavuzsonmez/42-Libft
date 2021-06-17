a1a209ca2df904e70131a1e9f3178d3df587cece
yavuzsonmez <contact@yavuzsonmez.com>
2021-06-17 13:04:09 +0200

    libft

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,22 +6,20 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 08:36:11 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/06/17 11:44:53 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/06/17 13:03:58 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-#include <stdio.h>
-
-void *malloc(size_t size);
+void	*malloc(size_t size);
 
 static int	count_digit(unsigned int n)
 {
-	size_t i;
+	size_t	i;
 
 	i = 1;
-	while(n >= 10)
+	while (n >= 10)
 	{
 		n /= 10;
 		i++;
@@ -31,45 +29,49 @@ static int	count_digit(unsigned int n)
 
 static char	get_char(unsigned int n)
 {
-	char c;
+	char	c;
 
 	c = n % 10 + 48;
-	//printf("get_char : %c\n", c);
 	return (c);
 }
 
-char		*ft_itoa(int n)
+static char	*get_s(size_t digit, unsigned int number, char *s)
+{
+	int	i;
+
+	i = digit - 1;
+	while (i >= 0)
+	{
+		s[i] = get_char(number);
+		number /= 10;
+		i--;
+	}
+	return (s);
+}
+
+char	*ft_itoa(int n)
 {
 	char			*s;
-	size_t			i;
 	size_t			digit;
 	unsigned int	number;
 
 	s = NULL;
 	if (n < 0)
+	{
 		number = n * (-1);
+		digit = count_digit(number) + 1;
+	}
 	else
+	{
 		number = n;
-	digit = count_digit(number) + 1;
-	if(!(s = (char *)malloc(sizeof(char) * (digit + 1))))
+		digit = count_digit(number);
+	}
+	s = (char *)malloc(sizeof(char) * (digit + 1));
+	if (!s)
 		return (NULL);
+	s = get_s(digit, number, s);
 	if (n < 0)
 		s[0] = 45;
-	i = digit - 1;
-	while(i > 0)
-	{
-		s[i] = get_char(number);
-		printf("%c\n", s[i]);
-		number /= 10;
-		i--;
-	}
 	s[digit] = 0;
 	return (s);
 }
-
-int main (void)
-{
-	printf("itoa return = %s\n", ft_itoa(247));
-	return (0);
-}
-
