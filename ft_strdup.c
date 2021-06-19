0dbcc12ee7652a6ec15644e4797b5965efb7e0fc
yavuzsonmez <contact@yavuzsonmez.com>
2021-06-19 10:20:30 +0200

    libft

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,29 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 15:51:14 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/06/17 15:51:21 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/06/19 10:20:15 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+#include "libft.h"
+
+void	*malloc(size_t size);
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	char	*ss;
+
+	i = 0;
+	ss = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!ss)
+		return (NULL);
+	while (s[i])
+	{
+		ss[i] = s[i];
+		i++;
+	}
+	ss[i] = 0;
+	return (ss);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,24 +6,36 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 17:40:11 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/06/17 15:51:54 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/06/19 10:00:58 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
+
 #include <stddef.h>
+#include "libft.h"
+
+void	*malloc(size_t size);
+
 #include <stdlib.h>
+#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ss;
 	size_t	i;
+	size_t	count;
+	char	*ss;
 
-	ss = NULL;
 	i = 0;
-	if (ss = (char *)malloc(sizeof(char) * (len + 1)))
+	if(!s)
 		return (NULL);
-	while(s[start + i] && i < len)
+	if (ft_strlen(s + start) >= len)
+		count = len;
+	else
+		count = ft_strlen(s + start);
+	ss = (char *)malloc(sizeof(char) * (count + 1));
+	if (!ss)
+		return (NULL);
+	while(i < count)
 	{
 		ss[i] = s[start + i];
 		i++;
@@ -33,13 +45,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 }
 
 
-#include <stdio.h>
-
 int main()
 {
-
-	printf("%s", ft_substr("Hello", 2, 3));
+	printf("%s", ft_substr("bonjour", 8, 4));
 	return 0;
 }
 
-*/
