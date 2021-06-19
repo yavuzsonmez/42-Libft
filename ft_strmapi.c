9e9cd07325d34e1348157a74efbbe5190b58af19
yavuzsonmez <contact@yavuzsonmez.com>
2021-06-19 11:48:43 +0200

    libft

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,33 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 15:51:35 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/06/17 15:51:36 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/06/19 11:48:20 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+#include "libft.h"
+
+void	*malloc(size_t size);
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*ms;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	ms = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ms)
+		return (NULL);
+	while (i < len)
+	{
+		ms[i] = f(i, s[i]);
+		i++;
+	}
+	ms[i] = 0;
+	return (ms);
+}
