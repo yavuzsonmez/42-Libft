6e390dafb0569f2decef547f532bcd55f5cea36c
Yavuz Sönmez <contact@yavuzsonmez.com>
2021-06-20 21:55:33 +0200

    libft

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -11,14 +11,23 @@
 /* ************************************************************************** */
 
 /*
-
-#include <stddef.h>
-
+#include "libft.h"
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t i;
 
+  i = 0;
+  if (src[ft_strlen(src)] != 0)
+    return (NULL);
+  while(i < size)
+  {
+      dst[i] = src[i];
+      i++;
+  }
+  dst[i] = 0;
 
 }
 
+
+bonjou
 */
