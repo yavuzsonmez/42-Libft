668a6595e4f9a40d61648c3af5f08b4a20322067
yavuzsonmez <contact@yavuzsonmez.com>
2021-06-21 13:52:05 +0200

    libft

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,14 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 11:57:37 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/06/21 11:48:54 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/06/21 13:51:57 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+#include <string.h>
+#include <stdio.h>
 
 static size_t	count_str(char const *s, char const c)
 {
@@ -19,28 +22,35 @@ static size_t	count_str(char const *s, char const c)
 
 	i = 0;
 	str = 0;
-	while(s[i])
+	while (s[i])
 	{
-		if(s[++i] && s[i] == c)
+		if (s[i] == c && i > 0 && s[i - 1] != c)
 			str++;
 		i++;
 	}
 	return (str);
 }
 
-static char *	newstr(char const *s, char c)
+//static size_t	count_char(char const *s, char const c)
+//{
+//	static size_t	i;
+//}
+
+static char	*newstr(char const *s, char c)
 {
 	static size_t	i;
 	size_t			j;
+	size_t			k;
 	size_t			count;
 	char			*str;
 
 	i = 0;
 	count = 0;
-	j = i;
-	if (s[i] == c)
+	k = 0;
+	while (s[i] == c)
 		i++;
-	while(s[i] && s[i] != c)
+	j = i;
+	while (s[i] && s[i] != c)
 	{
 		count++;
 		i++;
@@ -48,33 +58,53 @@ static char *	newstr(char const *s, char c)
 	str = (char *)malloc(sizeof(char) * (count + 1));
 	if (!str)
 		return (NULL);
-	while (j < count)
+	j = i + 1;
+	while (k < count)
 	{
-		str[j] = s[j];
+		str[k] = s[j];
 		j++;
+		k++;
 	}
-	str[j] = 0;
+	str[k] = 0;
 	return (str);
 }
 
-char			**ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	c_str;
 	char	**arr;
 
 	i = 0;
-	if(!s)
+	if (!s)
 		return (NULL);
 	c_str = count_str(s, c);
 	arr = (char **)malloc(sizeof(char *) * (c_str + 1));
 	if (!arr)
 		return (NULL);
-	while(i < c_str)
+	while (i < c_str)
 	{
 		arr[i] = newstr(s, c);
 		i++;
 	}
 	arr[i] = NULL;
 	return (arr);
+}
+
+
+int main(void)
+{
+	int i = 0;
+	char const c = 'd';
+	char const *s;
+
+	s = strdup("BonjourdBonjourd");
+	char **arr = ft_split(s, c);
+
+	while(arr[i])
+	{
+		printf("%s\n", arr[i]);
+		i++;
+	}
+	return 0;
 }
